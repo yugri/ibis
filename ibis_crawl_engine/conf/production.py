6d1f27778ca76a23ca7e2482256c36c601f1ee47
Yuri Gritsai <yugritsai@gmail.com>
2016-07-13 08:26:23 +0300

    settings for prod

--- ibis_crawl_engine/conf/production.py
+++ ibis_crawl_engine/conf/production.py
@@ -1,7 +1,7 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = False
+DEBUG = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip("/"))
