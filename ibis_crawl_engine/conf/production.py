1732c3d5b7d85a2543e5a38bd4b835b02515c508
Yuri Gritsai <yugritsai@gmail.com>
2016-07-14 12:11:30 +0300

    settings update

--- ibis_crawl_engine/conf/production.py
+++ ibis_crawl_engine/conf/production.py
@@ -4,7 +4,7 @@ from django.utils import six
 
 from ibis_crawl_engine.conf import *
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 DEBUG = False
 
