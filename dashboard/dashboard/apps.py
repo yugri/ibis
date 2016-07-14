0fb3c78d278a1ac75ced123917dd840aeac6cbd6
Yuri Gritsai <yugritsai@gmail.com>
2016-07-14 12:51:04 +0300

    settings update

--- dashboard/dashboard/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DashboardConfig(AppConfig):
-    name = 'dashboard'
-    verbose_name = "Dashboard"
--- dashboard/dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from dashboard.views import IndexView
-
-urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
-]
