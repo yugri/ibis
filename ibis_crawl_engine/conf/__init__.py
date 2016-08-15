ea45ce70997e9ba3cd696e8e879c343bc091a997
Yuri Gritsai <yugritsai@gmail.com>
2016-08-15 23:36:18 +0300

    using Redis as broker & results backend

--- ibis_crawl_engine/conf/__init__.py
+++ ibis_crawl_engine/conf/__init__.py
@@ -27,8 +27,13 @@ MONGODB_COLLECTION = "articles"
 ###################
 # Celery settings #
 ###################
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
-CELERY_RESULT_BACKEND = 'mongodb://localhost:27017/'
+# CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'mongodb://localhost:27017/'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 CELERY_MONGODB_BACKEND_SETTINGS = {
     'database': MONGODB_DB,
     'taskmeta_collection': 'task_meta',
