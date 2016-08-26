92602e42b5d74c4ffbe79d7d65aa42a40433e7e6
Yuri Gritsai <yugritsai@gmail.com>
2016-08-26 15:54:22 +0300

    db user ERROR fixing

--- ibis_crawl_engine/conf/production.py
+++ ibis_crawl_engine/conf/production.py
@@ -21,7 +21,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'crawlerdb',
-        'USER': 'crawler',
+        'USER': 'ibis',
         'PASSWORD': '840402136314',
         'HOST': 'localhost',
         'PORT': ''
