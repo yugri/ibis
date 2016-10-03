81d578b0d93ed2d966cca36b6d260407e882665b
Yuri Gritsai <yugritsai@gmail.com>
2016-10-03 18:24:35 +0300

    search_id UUID fixed

--- crawl_engine/serializers.py
+++ crawl_engine/serializers.py
@@ -24,8 +24,7 @@ class ArticleTransferSerializer(serializers.ModelSerializer):
         # exclude = ('search', 'pushed')
 
     def get_search_id(self, obj):
-        # return '46268d29-ebff-4614-b045-f28bc673f6cf'
-        return '5c15f2a9-b89a-4c0c-b7b5-b0eb38607b2c'
+        return '46268d29-ebff-4614-b045-f28bc673f6cf'
 
 
 
