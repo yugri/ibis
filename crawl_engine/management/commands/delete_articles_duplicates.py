000fea7c8026b9cb2bfc205fef0935f5354d5251
Yuri Gritsai <yugritsai@gmail.com>
2016-12-15 14:30:25 +0200

    manage command for deleting articles duplicates

--- crawl_engine/management/commands/delete_articles_duplicates.py
+++ crawl_engine/management/commands/delete_articles_duplicates.py
@@ -10,16 +10,12 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = 'Deletes all article objects duplicates from given database'
 
-    def add_arguments(self, parser):
-        parser.add_argument('db_name', type=str)
-
     def handle(self, *args, **options):
-        database = options['db_name']
-        duplicate_articles = Article.objects.using(database).values('article_url').annotate(
+        duplicate_articles = Article.objects.values('article_url').annotate(
             url_count=Count('article_url')
         ).filter(url_count__gt=1)
         for article in duplicate_articles:
-            a = Article.objects.using('articles').filter(article_url=article['article_url'])[0]
+            a = Article.objects.filter(article_url=article['article_url'])[0]
             a_id = a.id
             a.delete()
             self.stdout.write(self.style.SUCCESS('Article duplicate %s DELETED' % a_id))
