275627bfea899321548c30d58c8279c186920d4a
vovangan <vovangan@gmail.com>
2017-04-17 18:12:03 +0700

    new settings file

--- manage.py
+++ manage.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-import os
-import sys
+import os, sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ibis_crawl_engine.settings")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ibis_crawl_engine.settings.common')
 
     from django.core.management import execute_from_command_line
 
