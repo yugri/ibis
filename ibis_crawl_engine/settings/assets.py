9ccb56b7e27bab81ac4a9ad862ae48952f956df9
vovangan <vovangan@gmail.com>
2017-04-17 17:50:12 +0700

    settings in new dir

---
+++ ibis_crawl_engine/settings/assets.py
@@ -0,0 +1,18 @@
+PIPELINE = {
+    'STYLESHEETS': {
+        'project_css': {
+            'source_filenames': {
+                'css/project.css',
+            },
+            'output_filename': 'css/project.css',
+        }
+    },
+    'JAVASCRIPT': {
+        'project_js': {
+            'source_filenames': {
+                'js/project.js',
+            },
+            'output_filename': 'js/project.js',
+        }
+    }
+}
