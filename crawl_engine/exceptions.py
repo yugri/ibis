582a6c5c5c68441913a73dd96e8747e2736b6ec4
denys <denys.potapov@gmail.com>
2017-04-25 15:02:01 +0300

    remove blacklisted exception

--- crawl_engine/exceptions.py
+++ crawl_engine/exceptions.py
@@ -4,9 +4,3 @@ class Error(Exception):
     def __init__(self, message, *args):
         # Set some exception infomation
         self.msg = message
-
-
-class BlacklistedURLException(Error):
-    def __init__(self, resource, *args):
-        self.resource = resource
-        super(BlacklistedURLException, self).__init__(resource, *args)
